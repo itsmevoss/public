3826acfd244bfbdd88d3fcbc2978d8a214bc5e65
itsmevoss <aaron@thevoss.es>
2023-05-05 11:12:03 -0500

    Create scansetup.sh
    initial commit

---
+++ security/scansetup.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install wireguard
+wg genkey | sudo tee /etc/wireguard/private.key
+sudo chmod go= /etc/wireguard/private.key
+sudo cat /etc/wireguard/private.key | wg pubkey | sudo tee /etc/wireguard/public.key
